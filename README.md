507eb2fa4fa18d6c3bd1cace222ba4ec298e1ada
WildR0s3 <pawel.hajdecki88+github@gmail.com>
2026-06-10 12:30:46 +0200

    comments

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 Dataset used is 'Oxford-IIIT Pet dataset' available at https://www.robots.ox.ac.uk/~vgg/data/pets/
 
 # Traning
-Traning this model for 10 epochs on 'Oxford-IIIT Pet dataset' resulted in below evaluation
---- Epoch 10/10 ---
-Loss: 0.2050
-Pixel Accuracy: 92.37%
-Dice Score (Macro): 0.8660
+Traning this model for 10 epochs on 'Oxford-IIIT Pet dataset' resulted in below evaluation <br>
+--- Epoch 10/10 --- <br>
+Loss: 0.2050 <br>
+Pixel Accuracy: 92.37% <br>
+Dice Score (Macro): 0.8660 <br>
 
 # Visual evaluation
 There are 2 mask being scaled to visible, actual and predicted of Ragdoll_171 picture.
--- dataset.py
+++ dataset.py
@@ -13,18 +13,20 @@ class SegmentationDataset(Dataset):
         self.images = sorted(os.listdir(image_dir))
         self.masks = sorted(os.listdir(mask_dir))
 
+        # sort for alginment between masks and outputs
         self.images = sorted([f for f in os.listdir(image_dir) if not f.startswith('._')])
         self.masks = sorted([f for f in os.listdir(mask_dir) if not f.startswith('._')])
         
         # Geometric and color transforms for images
         self.img_transform = T.Compose([
             T.Resize(image_size),
-            T.ToTensor(),
-            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            T.ToTensor(), # [Height, Width, 3], channel have to come first
+            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # averages of ImageNet for statistical baseline
         ])
         
         # Mask transforms (Must remain Nearest Neighbor to avoid changing class values)
         self.mask_transform = T.Compose([
+             # standarize to 256x256 by using Nearest Neighbour Interpolation to keep 3 fixed classes / pixel values
             T.Resize(image_size, interpolation=T.InterpolationMode.NEAREST),
             T.Lambda(self.convert_mask_to_tensor)
         ])
@@ -45,4 +47,5 @@ class SegmentationDataset(Dataset):
         return image, mask
     
     def convert_mask_to_tensor(self, img):
+        """make sure you preserve distinction between pixel values 1,2,3"""
         return torch.from_numpy(np.array(img)).long() - 1
